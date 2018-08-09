2e8bfb4c5a263c85464b37c289772e6db252bb8d
Fintan McGee <fintanmcgee1981@gmail.com>
2018-08-09 11:26:40 +0100

    Create Behaviour.js

---
+++ Behaviour.js
@@ -0,0 +1,13 @@
+
+setTimeout(wakeUpUser, 1500);
+function wakeUpUser() {
+  alert("Are you going to just stare?");
+  alert("Make me a sandwich!");
+
+}
+
+var x = prompt("Are you behaving?");
+if (x === "") {
+  alert("Wrong answer!");
+
+}
